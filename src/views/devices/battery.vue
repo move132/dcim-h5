24d82c100e2a81724848e07dd38089ffa73a0c9e
wanmin <min.wan@sinocare.com>
2019-10-20 16:29:23 +0800

    [*]优化代码

--- src/views/devices/battery.vue
+++ src/views/devices/battery.vue
@@ -2,101 +2,79 @@
 <!-- 蓄电池 -->
     <div>
         <div class="battery">
-            <div class="info">
-                <div class="fild">
-                    <div>
-                        <span class="label">电池电压：</span>
-                        <span class="val">220v</span>
+            <template v-if="dataList.length > 0">
+                <div class="info">
+                    <div class="fild">
+                        <div>
+                            <span class="label">电池电压：</span>
+                            <span class="val">{{dataList[0].dy?dataList[0].dy:'--'}}</span>
+                        </div>
+                        <div>
+                            <span class="label">温度：</span>
+                            <span class="val">{{dataList[0].wd?dataList[0].wd:'--'}}</span>
+                        </div>
                     </div>
-                    <div>
-                        <span class="label">温度1：</span>
-                        <span class="val">100</span>
+                    <div class="fild">
+                        <div>
+                            <span class="label">电池电流：</span>
+                            <span class="val">{{dataList[0].dl?dataList[0].dl:'--'}}</span>
+                        </div>
+                        <div>
+                            <span class="label">内阻：</span>
+                            <span class="val">{{dataList[0].nz?dataList[0].nz:'--'}}</span>
+                        </div>
                     </div>
                 </div>
-                <div class="fild">
-                    <div>
-                        <span class="label">电池电流：</span>
-                        <span class="val">20v</span>
-                    </div>
-                    <div>
-                        <span class="label">温度2：</span>
-                        <span class="val">80</span>
-                    </div>
-                </div>
-            </div>
-            <van-collapse v-model="activeNames">
-                <van-collapse-item :title="item.name" :name="index + 1" v-for="(item, index) in dataList" :key="index">
-                    <div class="coll-list">
-                        <template v-if="item.type === '1'">
-                            <div class="item" v-for="(subitem, num) in item.singleData" :key="num">
-                                <span class="name">{{subitem.depaVname}}</span>
-                                <van-tag type="primary">{{subitem.value}}</van-tag>
-                                <span class="unit" v-if="subitem.unit">V</span>
-                            </div>
-                        </template>
-                        <template>
-                            <template v-for="(subitem, num) in item.multiData">
-                                <div class="item" >
-                                    <span class="name">电压{{num+1}}</span>
-                                    <van-tag type="primary">{{subitem.dy}}</van-tag>
-                                    <span class="unit" v-if="subitem.unit">V</span>
-                                </div>
-                                <div class="item">
-                                    <span class="name">内阻{{num+1}}</span>
-                                    <van-tag type="primary">{{subitem.nz}}</van-tag>
-                                    <span class="unit" v-if="subitem.unit">V</span>
-                                </div>
-                                <div class="item">
-                                    <span class="name">温度{{num+1}}</span>
-                                    <van-tag type="primary">{{subitem.wd}}</van-tag>
+                <van-collapse v-model="activeNames">
+                    <van-collapse-item :title="item.name" :name="index + 1" v-for="(item, index) in dataList" :key="index">
+                        <div class="coll-list">
+                            <template v-if="item.type === '1'">
+                                <div class="item" v-for="(subitem, num) in item.singleData" :key="num">
+                                    <span class="name">{{subitem.depaVname}}</span>
+                                    <van-tag type="primary">{{subitem.value}}</van-tag>
                                     <span class="unit" v-if="subitem.unit">V</span>
                                 </div>
                             </template>
+                            <template>
+                                <template v-for="(subitem, num) in item.multiData">
+                                    <div class="item" :key="num+'dy'" >
+                                        <span class="name">电压{{num+1}}</span>
+                                        <van-tag type="primary">{{subitem.dy}}</van-tag>
+                                        <span class="unit" v-if="subitem.unit">V</span>
+                                    </div>
+                                    <div class="item" :key="num+'nz'">
+                                        <span class="name">内阻{{num+1}}</span>
+                                        <van-tag type="primary">{{subitem.nz}}</van-tag>
+                                        <span class="unit" v-if="subitem.unit">V</span>
+                                    </div>
+                                    <div class="item" :key="num+'wd'">
+                                        <span class="name">温度{{num+1}}</span>
+                                        <van-tag type="primary">{{subitem.wd}}</van-tag>
+                                        <span class="unit" v-if="subitem.unit">V</span>
+                                    </div>
+                                </template>
 
-                        </template>
-                    </div>
-                </van-collapse-item>
-               <!--  <van-collapse-item title="2号电池组" name="2">
-                    <div class="coll-list">
-                        <div class="item" v-for="(item, index) in [1,2,3,4,5,6]" :key="index">
-                            <span class="name">第{{item}}节电池电压</span>
-                            <van-tag type="primary">22.3</van-tag>
-                            <span class="unit" v-if="item">V</span>
+                            </template>
                         </div>
-                    </div>
-                </van-collapse-item>
-                <van-collapse-item title="3号电池组" name="3">
-                    <div class="coll-list">
-                        <div class="item" v-for="(item, index) in [1,2,3,4,5,6]" :key="index">
-                            <span class="name">第{{item}}节电池电压</span>
-                            <van-tag type="primary">82.3</van-tag>
-                            <span class="unit" v-if="item">V</span>
-                        </div>
-                    </div>
-                </van-collapse-item>
-                <van-collapse-item title="4号电池组" name="4">
-                    <div class="coll-list">
-                        <div class="item" v-for="(item, index) in [1,2,3,4,5,6]" :key="index">
-                            <span class="name">第{{item}}节电池电压</span>
-                            <van-tag type="primary">12.3</van-tag>
-                            <span class="unit" v-if="item">V</span>
-                        </div>
-                    </div>
-                </van-collapse-item> -->
-            </van-collapse>
+                    </van-collapse-item>
+                </van-collapse>
+            </template>
+            <Nodata v-else></Nodata>
         </div>
     </div>
 </template>
 
 <script>
+import Nodata from '@/components/Nodata';
 export default {
     data() {
         return {
-            activeNames: ['1'],
+            activeNames: [1],
             dataList: []
         };
     },
     components: {
+        Nodata
     },
     created() {
         this.queryParam = this.$router.currentRoute.query;
