c75b4468ca03805931135c7788294c4218c6cc53
wanmin <min.wan@sinocare.com>
2020-01-16 19:10:19 +0800

    [*]优化

--- src/views/devices/monitorvideo.vue
+++ src/views/devices/monitorvideo.vue
@@ -7,6 +7,14 @@
                 <!-- <video src="http://hls01open.ys7.com/openlive/2c0641b9fe0d47ea93ebc2e381869b80.hd.m3u8" controls="controls" webkit-playsinline="" id="videoPlay" style="width:100%; height: 260px;"></video> -->
                 <iframe  :src="item.deviUrl+'?autoplay=1'"  style="height:200px; width: 100%;"  id="ysOpenDevice" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" allowfullscreen></iframe>
             </div>
+            <div class="video-item" v-for="(item ,index) in dataList" :key="index">
+                <!-- <video src="http://hls01open.ys7.com/openlive/2c0641b9fe0d47ea93ebc2e381869b80.hd.m3u8" controls="controls" webkit-playsinline="" id="videoPlay" style="width:100%; height: 260px;"></video> -->
+                <iframe  :src="item.deviUrl+'?autoplay=1'"  style="height:200px; width: 100%;"  id="ysOpenDevice" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" allowfullscreen></iframe>
+            </div>
+            <div class="video-item" v-for="(item ,index) in dataList" :key="index">
+                <!-- <video src="http://hls01open.ys7.com/openlive/2c0641b9fe0d47ea93ebc2e381869b80.hd.m3u8" controls="controls" webkit-playsinline="" id="videoPlay" style="width:100%; height: 260px;"></video> -->
+                <iframe  :src="item.deviUrl+'?autoplay=1'"  style="height:200px; width: 100%;"  id="ysOpenDevice" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
 </template>
@@ -52,7 +60,6 @@ export default {
         height: 200px;
         width: 100%;
         background: #666;
-        margin: 1%;/* 1% 0 0; */
     }
  }
 </style>
