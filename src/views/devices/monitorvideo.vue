3e123f37e5e42a1e102662932d7ac026c37b381e
wanmin <min.wan@sinocare.com>
2020-01-16 18:57:42 +0800

    [*]优化

--- src/views/devices/monitorvideo.vue
+++ src/views/devices/monitorvideo.vue
@@ -4,7 +4,7 @@
         <div class="video-list">
             <!-- <div class="video-item" v-for="(item ,index) in 'dataLis'" :key="index"></div> -->
             <div class="video-item" v-for="(item ,index) in dataList" :key="index">
-                <!-- <video src="https://open.ys7.com/ezopen/h5/live?autoplay=1&audio=1&accessToken=at.8fdi08no85t2bfkw493l6im6bg7ilwy2-9guiyp58c1-18m751o-g9ndqalhs&hd=1&deviceSerial=203751922&channelNo=1" controls="controls" webkit-playsinline="" id="videoPlay" style="width:100%; height: 260px;"></video> -->
+                <!-- <video src="http://hls01open.ys7.com/openlive/2c0641b9fe0d47ea93ebc2e381869b80.hd.m3u8" controls="controls" webkit-playsinline="" id="videoPlay" style="width:100%; height: 260px;"></video> -->
                 <iframe  :src="item.deviUrl+'?autoplay=1'"  width="100%"  height="300"  id="ysOpenDevice"  allowfullscreen></iframe>
             </div>
         </div>
@@ -47,12 +47,12 @@ export default {
  .video-list {
     display: flex;
     flex-wrap: wrap;
-    margin-left: 1%;
+    /* margin-left: 1%; */
     .video-item {
-        height: 100px;
-        width: 49%;
+        /* height: 100px; */
+       /*  width: 49%; */
         background: #666;
-        margin: 1% 1% 0 0;
+        margin: 1%;/* 1% 0 0; */
     }
  }
 </style>
