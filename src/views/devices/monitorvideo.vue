9bdcef3ca194b74bfe5341a06da58dc656cedfc3
wanmin <min.wan@sinocare.com>
2020-01-16 19:15:33 +0800

    [*]优化

--- src/views/devices/monitorvideo.vue
+++ src/views/devices/monitorvideo.vue
@@ -2,16 +2,9 @@
     <!-- 照明 -->
     <div class="video">
         <div class="video-list">
-            <!-- <div class="video-item" v-for="(item ,index) in 'dataLis'" :key="index"></div> -->
             <div class="video-item" v-for="(item ,index) in dataList" :key="index">
                 <!-- <video src="http://hls01open.ys7.com/openlive/2c0641b9fe0d47ea93ebc2e381869b80.hd.m3u8" controls="controls" webkit-playsinline="" id="videoPlay" style="width:100%; height: 260px;"></video> -->
-                <iframe  :src="item.deviUrl"  style="height:200px; width: 100%;"  id="ysOpenDevice" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" allowfullscreen></iframe>
-            </div>
-            <div class="video-item" v-for="(item ,index) in dataList" :key="index">
-                <iframe  :src="item.deviUrl"  style="height:200px; width: 100%;"  id="ysOpenDevice" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" allowfullscreen></iframe>
-            </div>
-            <div class="video-item" v-for="(item ,index) in dataList" :key="index">
-                <iframe  :src="item.deviUrl"  style="height:200px; width: 100%;"  id="ysOpenDevice" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" allowfullscreen></iframe>
+                <iframe  :src="item.deviUrl+'?autoplay=1'"  style="height:200px; width: 100%;"  id="ysOpenDevice" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" allowfullscreen></iframe>
             </div>
         </div>
     </div>
@@ -56,8 +49,8 @@ export default {
     /* margin-left: 1%; */
     .video-item {
         height: 200px;
+        margin-bottom: 5px;
         width: 100%;
-        margin-bottom:5px;
         background: #666;
     }
  }
